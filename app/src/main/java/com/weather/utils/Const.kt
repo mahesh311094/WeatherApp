f5a6277c212b8a3ba2d1c06a837c6fedeabd187a
Mahesh <mahesh311094@gmail.com>
2022-08-25 11:16:24 +0530

    init commit

--- app/src/main/java/com/weather/utils/Const.kt
+++ app/src/main/java/com/weather/utils/Const.kt
@@ -2,6 +2,6 @@ package com.weather.utils
 
 object Const {
     const val BASE_URL = "https://api.openweathermap.org/data/"
-    const val APP_ID = ""
+    const val APP_ID = "" // enter api key from openweathermap
     const val ERROR_MESSAGE = "Error please try again later"
 }
